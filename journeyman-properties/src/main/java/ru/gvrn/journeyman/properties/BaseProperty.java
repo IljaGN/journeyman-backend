156dfe678134ec9c26041fdbd94f98d3cbb61a7a
IlyaGovorun <govoruniljan@gmail.com>
2024-09-09 19:54:44 +0300

    Add logic in class BaseProperty

--- journeyman-properties/src/main/java/ru/gvrn/journeyman/properties/BaseProperty.java
+++ journeyman-properties/src/main/java/ru/gvrn/journeyman/properties/BaseProperty.java
@@ -1,4 +1,53 @@
 package ru.gvrn.journeyman.properties;
 
-public abstract class BaseProperty {
+import lombok.Getter;
+import ru.gvrn.journeyman.properties.api.Property;
+import ru.gvrn.journeyman.properties.api.Value;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class BaseProperty<T> implements Property<T> {
+  @Getter // TODO: interface UniqueName
+  private final String name;
+  protected final Map<String, Value<T>> values;
+  protected final Value<T> defolt;
+  protected final Value<T> current;
+
+  public BaseProperty(String name, Value<T> value) {
+    this.name = name;
+    value.setOwnerName(name);
+    values = new HashMap<>();
+    current = value;
+    defolt = initValue("default");
+    addValues(current, defolt);
+  }
+
+  @Override
+  public T getValue() {
+    return current.getValue();
+  }
+
+  @Override
+  public void replaceValue(T value) {
+    current.setValue(value);
+  }
+
+  @Override
+  public void resetOnDefault() {
+    current.setValue(defolt.getValue());
+  }
+
+  @Override
+  public void updateDefaultOnCurrent() {
+    defolt.setValue(current.getValue());
+  }
+
+  protected Value<T> initValue(String name) {
+    return current.getInstance(name);
+  }
+
+  protected void addValues(Value<T>... values) {
+//    Arrays.stream(values).forEach(v -> this.values.put(v.getName(), v));
+  }
 }
