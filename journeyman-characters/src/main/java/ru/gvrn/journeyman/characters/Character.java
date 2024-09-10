b82ba3446eb9674fd2d03c6c4e8c6a9debefb4b2
IlyaGovorun <govoruniljan@gmail.com>
2024-09-10 15:46:33 +0300

    Add characteristics in Character

--- journeyman-characters/src/main/java/ru/gvrn/journeyman/characters/Character.java
+++ journeyman-characters/src/main/java/ru/gvrn/journeyman/characters/Character.java
@@ -1,4 +1,16 @@
 package ru.gvrn.journeyman.characters;
 
+import ru.gvrn.journeyman.properties.api.Property;
+
+import java.util.Map;
+
 public class Character {
+  protected final Map<String, Property<?>> characteristics;
+
+//  protected final Map<String, Item> items = new HashMap<>();
+//  protected final Body body;
+
+  public Character(Map<String, Property<?>> characteristics) {
+    this.characteristics = characteristics;
+  }
 }
