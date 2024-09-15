87648042930b7010e7ae9861c42bf10444ba503a
IlyaGovorun <govoruniljan@gmail.com>
2024-09-15 21:29:24 +0300

    Update Character class

--- journeyman-characters/src/main/java/ru/gvrn/journeyman/characters/Character.java
+++ journeyman-characters/src/main/java/ru/gvrn/journeyman/characters/Character.java
@@ -1,16 +1,43 @@
 package ru.gvrn.journeyman.characters;
 
+import ru.gvrn.journeyman.items.Item;
+import ru.gvrn.journeyman.items.OutfitItem;
+import ru.gvrn.journeyman.outfits.api.Outfitter;
 import ru.gvrn.journeyman.properties.api.Property;
+import ru.gvrn.journeyman.properties.types.IntegerProperty;
+import ru.gvrn.journeyman.properties.types.IntegerRestrictedProperty;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Character {
+public class Character implements Outfitter<OutfitItem> {
   protected final Map<String, Property<?>> characteristics;
 
-//  protected final Map<String, Item> items = new HashMap<>();
-//  protected final Body body;
+  protected final Map<String, Item> items = new HashMap<>();
+  protected final Body body;
 
-  public Character(Map<String, Property<?>> characteristics) {
+  private IntegerRestrictedProperty carryingCapacity;
+  private IntegerProperty armorClass;
+
+  public Character(Map<String, Property<?>> characteristics, Body body) {
     this.characteristics = characteristics;
+    this.body = body;
+    carryingCapacity = (IntegerRestrictedProperty) characteristics.get("Carrying Capacity");
+    armorClass = (IntegerProperty) characteristics.get("Armor Class");
+  }
+
+  @Override
+  public List<OutfitItem> equip(List<OutfitItem> outfits) {
+//    items.addAll(outfits);
+//    carryingCapacity.replaceValue(items.stream()
+//        .mapToInt(Item::getWeight).sum()
+//    );
+    return body.equip(outfits);
+  }
+
+  @Override
+  public List<OutfitItem> unequip(List<String> ids) {
+    return body.unequip(ids);
   }
 }
