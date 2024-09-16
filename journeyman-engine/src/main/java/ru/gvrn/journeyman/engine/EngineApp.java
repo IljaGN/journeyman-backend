86418e8ac8613d5f32b4e8494d08acf9ac21a144
IlyaGovorun <govoruniljan@gmail.com>
2024-09-16 17:43:01 +0300

    Add and enable spring context

--- journeyman-engine/src/main/java/ru/gvrn/journeyman/engine/EngineApp.java
+++ journeyman-engine/src/main/java/ru/gvrn/journeyman/engine/EngineApp.java
@@ -1,4 +1,15 @@
 package ru.gvrn.journeyman.engine;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
 public class EngineApp {
+  public static void main(String[] args) {
+    System.out.println(EngineApp.class.getSimpleName() + "#main start");
+    new AnnotationConfigApplicationContext(EngineApp.class);
+    System.out.println(EngineApp.class.getSimpleName() + "#main finish");
+  }
 }
