45403613f5f296922b9152a3d790a20ca208dc96
IlyaGovorun <govoruniljan@gmail.com>
2024-12-03 03:32:46 +0300

    [TMP] Fix cyclic reference between journeyman 'properties-api' and 'support-api'
    This is a temporary solution and requires architectural revision.

--- journeyman-properties-api/src/main/java/ru/gvrn/journeyman/properties/api/Property.java
+++ journeyman-properties-api/src/main/java/ru/gvrn/journeyman/properties/api/Property.java
@@ -1,8 +1,9 @@
 package ru.gvrn.journeyman.properties.api;
 
-import ru.gvrn.journeyman.support.api.UniqueName;
+//import ru.gvrn.journeyman.support.api.UniqueName;
 
-public interface Property<T> extends UniqueName {
+public interface Property<T> { // extends UniqueName
+  String getName(); // TODO: problem <!-- FIXME: Циклическая ссылка --> in 'journeyman-support-api'
   T getValue();
   void replaceValue(T value);
   void resetOnDefault();
--- journeyman-properties-api/src/main/java/ru/gvrn/journeyman/properties/api/Value.java
+++ journeyman-properties-api/src/main/java/ru/gvrn/journeyman/properties/api/Value.java
@@ -1,8 +1,9 @@
 package ru.gvrn.journeyman.properties.api;
 
-import ru.gvrn.journeyman.support.api.UniqueName;
+//import ru.gvrn.journeyman.support.api.UniqueName;
 
-public interface Value<T> extends UniqueName {
+public interface Value<T> { // extends UniqueName
+  String getName(); // TODO: problem <!-- FIXME: Циклическая ссылка --> in 'journeyman-support-api'
   T getValue();
   void setValue(T value);
   Value<T> getInstance(String valueName); // Думаю нужно убрать из интерфейса
